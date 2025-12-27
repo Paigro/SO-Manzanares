8eb6e06a0dfd0a6c52dbdf292dc150a1f3da20cd
Andrés García Navarro <andrga37@ucm.es>
2025-12-27 13:06:31 +0100

    Ejercicio 2 hoja 3

---
+++ Apuntes Andres/ejercicio2.c
@@ -0,0 +1,21 @@
+// HOJA 3 EJERCICIO 2
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <linux/limits.h>
+
+int main(int argc, char *arv[]){
+	unsigned int pid= getpid();
+	printf("PID: %ui\nPPID: %ui\nPGID: %ui\nSID: %ui\n",
+		pid,
+		getppid(),
+		getpgid(pid),
+		getsid(pid));
+	char buf[PATH_MAX];
+	getcwd(buf, PATH_MAX);
+	printf("Directorio de trabajo: %s\nIdentificador de usuario: %ui\nIdentificador de grupo: %ui\n",
+		buf,
+		getuid(),
+		getgid());
+	return 0;
+}
